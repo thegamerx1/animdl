f1ddb3e0e3ffa4cd038734718a3ca75d474ce105
TheGamerX1 <34986564+thegamerx1@users.noreply.github.com>
2022-01-24 12:26:56 +0100

    fix new file error

--- animdl/core/codebase/helper/cf_clearance.py
+++ animdl/core/codebase/helper/cf_clearance.py
@@ -3,30 +3,32 @@ from configparser import ConfigParser
 config = ConfigParser()
 
 configFilePathVeryLongName = os.path.join(
-	os.getenv("APPDATA"), "AnimDL", "config.ini")
+    os.getenv("APPDATA"), "AnimDL", "config.ini")
 
 
 config.read(configFilePathVeryLongName)
 if not os.path.exists(configFilePathVeryLongName):
-	os.makedirs(os.path.dirname(configFilePathVeryLongName), exist_ok=True)
-	config.add_section('main')
+    os.makedirs(os.path.dirname(configFilePathVeryLongName), exist_ok=True)
+    config.add_section('main')
+    config.set('main', 'cf_clearance', "")
+    config.set('main', 'useragent', "")
 
 
 def query(session, useConfig):
-	if useConfig:
-		useragent = config.get("main", "useragent")
-		cf_clearance = config.get("main", "cf_clearance")
-	else:
-		useragent = input("user-agent: ")
-		cf_clearance = input("cf_clearance: ")
-		if useragent == "":
-			useragent = config.get("main", "useragent")
+    if useConfig:
+        useragent = config.get("main", "useragent")
+        cf_clearance = config.get("main", "cf_clearance")
+    else:
+        useragent = input("user-agent: ")
+        cf_clearance = input("cf_clearance: ")
+        if useragent == "":
+            useragent = config.get("main", "useragent")
 
-	session.cookies.set("cf_clearance", cf_clearance)
-	session.headers.update({"User-Agent": useragent})
+    session.cookies.set("cf_clearance", cf_clearance)
+    session.headers.update({"User-Agent": useragent})
 
-	config.set('main', 'cf_clearance', cf_clearance)
-	config.set('main', 'useragent', useragent)
+    config.set('main', 'cf_clearance', cf_clearance)
+    config.set('main', 'useragent', useragent)
 
-	with open(configFilePathVeryLongName, 'w') as f:
-		config.write(f)
+    with open(configFilePathVeryLongName, 'w') as f:
+        config.write(f)

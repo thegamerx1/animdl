2bd47fb1f4322f8fc5110ae3afc56de104b12223
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-02-16 13:02:01 +0545

    Optimisation: Use `lru_cache`

--- animdl/core/codebase/helper/uwu.py
+++ animdl/core/codebase/helper/uwu.py
@@ -1,5 +1,7 @@
-import regex
 import base64
+import functools
+
+import regex
 import yarl
 
 RECAPTCHA_API_JS = "https://www.google.com/recaptcha/api.js"
@@ -14,29 +16,32 @@ def bypass_ddos_guard(session, base_uri):
 
 
 def bypass_recaptcha(session, url, headers):
-
-    response = {}
-
-    initial_page = session.get(url, headers=headers)
-
     domain = (
         base64.b64encode("{0.scheme}://{0.host}:443".format(yarl.URL(url)).encode())
         .decode()
         .strip("=")
         + "."
     )
+    initial_page = session.get(url, headers=headers)
 
     site_key_match = regex.search(r"recaptchaSiteKey = '(.+?)'", initial_page.text)
+
+    if site_key_match is None:
+        return {}
+
+    response = {'token': get_token_recaptcha(session, domain, site_key_match.group(1), "{0.scheme}://{0.host}/".format(yarl.URL(url)))}
 
     number_match = regex.search(r"recaptchaNumber = '(\d+?)'", initial_page.text)
 
     if number_match:
         response.update(number=number_match.group(1))
 
-    if site_key_match is None:
-        return response
+    return response
 
-    recaptcha_site_key = site_key_match.group(1)
+
+@functools.lru_cache()
+def get_token_recaptcha(session, domain, recaptcha_site_key, url):
+
 
     recaptcha_out = session.get(
         RECAPTCHA_API_JS,
@@ -47,7 +52,7 @@ def bypass_recaptcha(session, url, headers):
     v_token_match = regex.search(r"releases/([^/&?#]+)", recaptcha_out)
 
     if v_token_match is None:
-        return response
+        return
 
     v_token = v_token_match.group(1)
     anchor_out = session.get(
@@ -66,11 +71,11 @@ def bypass_recaptcha(session, url, headers):
     recaptcha_token_match = regex.search(r'recaptcha-token.+?="(.+?)"', anchor_out)
 
     if recaptcha_token_match is None:
-        return response
+        return
 
     recaptcha_token = recaptcha_token_match.group(1)
 
-    token_response_out = session.post(
+    token_out = session.post(
         "https://www.google.com/recaptcha/api2/reload",
         params={"k": recaptcha_site_key},
         data={
@@ -84,10 +89,8 @@ def bypass_recaptcha(session, url, headers):
         headers={"referer": "https://www.google.com/recaptcha/api2"},
     ).text
 
-    token_match = regex.search(r'rresp","(.+?)"', token_response_out)
+    token_match = regex.search(r'rresp","(.+?)"', token_out)
     if token_match is None:
-        return response
+        return
 
-    response.update(token=token_match.group(1))
-
-    return response
+    return token_match.group(1)
